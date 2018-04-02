d9ecd975fdefd66b1e0a062883d0f580c4754b20
Matt Gohr <gohr89@gmail.com>
2018-04-02 19:22:33 -0400

    setting up burger api routs

--- routes/burger-api-routes.js
+++ routes/burger-api-routes.js
@@ -1,44 +1,43 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/authors", function(req, res) {
+  app.get("/api/burger", function(req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
-    db.Author.findAll({
+    db.Burger.findAll({
       include: [db.Post]
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+    }).then(function(dbBurger) {
+      res.json(dbBurger);
     });
   });
 
-  app.get("/api/authors/:id", function(req, res) {
+  app.get("/api/burger/:id", function(req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
-    db.Author.findOne({
+    db.Burger.findOne({
       where: {
         id: req.params.id
       },
-      include: [db.Post]
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+    }).then(function(dbBurger) {
+      res.json(dbBurger);
     });
   });
 
-  app.post("/api/authors", function(req, res) {
-    db.Author.create(req.body).then(function(dbAuthor) {
-      res.json(dbAuthor);
+  app.post("/api/burger", function(req, res) {
+    db.Burger.create(req.body).then(function(dbBurger) {
+      res.json(dbBurger);
     });
   });
 
-  app.delete("/api/authors/:id", function(req, res) {
-    db.Author.destroy({
+  app.delete("/api/burger/:id", function(req, res) {
+    db.Burger.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+    }).then(function(dbBurger) {
+      res.json(dbBurger);
     });
   });
 
